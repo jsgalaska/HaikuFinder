1335ece16961822a72e3f7e65ded44b81524bf18
jsgalaska <jsgalaska@gmail.com>
2015-03-17 15:00:17 -0400

    Upload Project

---
+++ src/Main.java
@@ -0,0 +1,135 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+
+public class Main {
+	protected ArrayList<Word> allWords = new ArrayList<Word>();
+	protected ArrayList<Word> firstLine = new ArrayList<Word>();
+	protected ArrayList<Word> secondLine = new ArrayList<Word>();
+	protected ArrayList<Word> thirdLine = new ArrayList<Word>();
+	private Word word;
+	private int currentLoc = 0;
+	private String everything;
+	
+	public Main() throws IOException{
+		readFile("TomSawyer.txt");
+		doStuff(everything);
+		checkHaiku(allWords);
+	}
+	public static void main(String[] args) throws IOException {
+	     Main run = new Main();
+	}
+
+	public void readFile(String fileName) throws IOException{
+		BufferedReader br = new BufferedReader(new FileReader(fileName));
+    	try {
+    		StringBuilder sb = new StringBuilder();
+    		String line = br.readLine();
+
+    		while (line != null) {
+    			sb.append(line);
+    			sb.append(' ');
+    			//sb.append(System.lineSeparator());
+    			//line = br.readLine();
+    			//parseString(line);
+    			line = br.readLine();
+    		}
+    		everything = sb.toString();
+    	} finally {
+    		br.close();
+    	}
+	}
+	
+	public void parseString(String line){
+		String currentWord;
+		StringTokenizer st = new StringTokenizer(line);
+	     while (st.hasMoreTokens()) {
+	         currentWord = st.nextToken();
+	         word = new Word(currentWord);
+	         firstLine.add(word);
+	     }
+	}
+	
+	public void printList(ArrayList<Word> list){
+		StringBuilder sb = new StringBuilder();
+		for(int i=0; i<list.size(); i++){
+			sb.append(list.get(i).toString());
+			//System.out.println(" "+list.get(i).toString()+" ");
+		}
+		String line = sb.toString();
+		System.out.println(line);
+	}
+	
+	public void doStuff(String message){
+		String[] test;
+		int i = 0;
+		test = everything.split(" ");
+		while(i<test.length){
+			word = new Word(test[i]);
+			allWords.add(word);
+			//System.out.println(word.getSyllables());
+			//System.out.println(test[i]);
+			i++;
+		}
+	}
+	
+	public void checkHaiku(ArrayList<Word> list){
+		int firstRow = 0;
+		int secondRow = 0;
+		int thirdRow = 0;
+		int j = 0;
+		//for(int i=0; i<list.size(); i++){
+			while(j<list.size()){
+				int temp = list.get(j).getSyllables();
+				if(firstRow<5){
+					System.out.println("<5");
+					if(firstRow+temp<=5){
+						firstLine.add(list.get(j));
+						firstRow=firstRow+temp;
+					}
+				}else if(firstRow==5&&secondRow<7){
+					System.out.println("<57");
+					if(secondRow+temp<=7){
+						secondLine.add(list.get(j));
+						secondRow=secondRow+temp;
+						}
+				}else if(firstRow==5&&secondRow==7&&thirdRow<5){
+					System.out.println("<575");
+					if(thirdRow+temp<=5){
+						thirdLine.add(list.get(j));
+						thirdRow=thirdRow+temp;
+					}
+				}else if(firstRow==5&&secondRow==7&&thirdRow==5){
+					System.out.println("Haiku");
+					printList(firstLine);
+					printList(secondLine);
+					printList(thirdLine);
+					firstLine.clear();
+					secondLine.clear();
+					thirdLine.clear();
+					firstRow=0;
+					secondRow=0;
+					thirdRow=0;
+					if(firstRow+temp<=5){
+						firstLine.add(list.get(j));
+						firstRow=firstRow+temp;
+					}
+				}else{
+					firstLine.clear();
+					secondLine.clear();
+					thirdLine.clear();
+					firstRow=0;
+					secondRow=0;
+					thirdRow=0;
+				}
+				j++;
+			}
+		//}
+		
+	}
+	
+	
+}
---
+++ src/Word.java
@@ -0,0 +1,54 @@
+
+public class Word {
+	private String word;
+	private int syllables;
+	private char previousVowel;
+	private int currentPOS;
+	private boolean vowelFound;
+	
+	public Word(String _word){
+		word = _word;
+		calcSyllables();
+	}
+	
+	public int getSyllables(){
+		return syllables;
+	}
+	
+	public String toString(){
+		return word+" ";
+	}
+	
+	private void calcSyllables(){
+		vowelFound=false;
+		for(int i=0; i<word.length(); i++){
+       	 currentPOS = i;
+       	 if(isVowel(word.charAt(i))&&vowelFound==false){
+       		 if(i==(word.length()-1)&&word.charAt(i)=='e'){
+       			 vowelFound = false;
+       		 }else{
+       			vowelFound = true;
+       		 syllables++; 
+       		 }
+       		 
+       	 }else{
+       		 vowelFound=false;
+       	 }
+        }
+	}
+	
+	private boolean endsWithE(String word){
+		if(word.charAt(word.length()-1)=='e'){
+			return true;
+		}
+		return false;
+	}
+	
+	private boolean isVowel(char c){
+		if(c=='a'||c=='e'||c=='i'||c=='o'||c=='u'||c=='y'){
+			previousVowel = c;
+			return true;
+			}
+		return false;
+	}
+}
